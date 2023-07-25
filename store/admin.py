4b0842b2d6adfadd5d2a9b97553ce6613ffcc921
Willian_Machado <willian.machado.av@gmail.com>
2023-07-25 19:20:32 -0300

    leve altecarao na lista de subcategorias dentro das categorias

--- store/admin.py
+++ store/admin.py
@@ -6,6 +6,7 @@ from store.models import Details, Product
 
 class DetailsAdmin(admin.StackedInline):
     model = Details
+    min_num = 1
 
 
 @admin.register(Product)
@@ -25,12 +26,3 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['status', 'is_new_colection']
     list_filter = ['category', 'subcategory', 'status', 'is_new_colection']
     search_fields = ['product_name', 'code']
-
-
-class ProductForm(forms.ModelForm):
-    class Meta:
-        model = Product
-        fields = '__all__'
-        widgets = {
-            'subcategory': forms.Select(choices=(('v', 'verdadeiro'), ('f', 'falso'))),
-        }
