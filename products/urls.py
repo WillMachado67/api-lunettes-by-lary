d487bc815955f0b1de98b289603becab4bf5bac2
Willian_Machado <willian.machado.av@gmail.com>
2023-08-21 19:09:31 -0300

    leve alteração na urls

--- products/urls.py
+++ products/urls.py
@@ -7,21 +7,21 @@ app_name = 'products'
 
 products_api_v1_router = SimpleRouter()
 products_api_v1_router.register(
-    'products',
+    'store/api/v1/products',
     api.ProductsApiv1ViewSet,
     basename='product-api',
 )
 
 new_colletion_api_v1_router = SimpleRouter()
 new_colletion_api_v1_router.register(
-    'new_collection',
+    'store/api/v1/new_collection',
     api.NewCollectionApiv1ViewSet,
     basename='new-collection-api',
 )
 
 feactured_products_api_v1_router = SimpleRouter()
 feactured_products_api_v1_router.register(
-    'featured_products',
+    'store/api/v1/featured_products',
     api.FeaturedProductsApiv1ViewSet,
     basename='featured-products-api',
 )
@@ -30,7 +30,8 @@ feactured_products_api_v1_router.register(
 urlpatterns = [
     path('', views.home, name='home'),
     path('get_subcategories', views.get_subcategories, name='get_subcategories'),
-    path('', include(products_api_v1_router.urls)),
-    path('', include(new_colletion_api_v1_router.urls)),
-    path('', include(feactured_products_api_v1_router.urls))
 ]
+
+urlpatterns += products_api_v1_router.urls
+urlpatterns += new_colletion_api_v1_router.urls
+urlpatterns += feactured_products_api_v1_router.urls
